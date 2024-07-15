e872c77bc0ee7f2248b4a98abd8ba4c618bbbe13
vashu-aggarwal <vashuaggarwal05@gmail.com>
2024-07-15 09:23:43 +0530

    Complete

---
+++ Day1/q2.cpp
@@ -0,0 +1,28 @@
+// 75. Sort Colors
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+            int low=-1,high=nums.size()-1,mid=0;
+            while(mid<=high)
+            {
+                
+                if(nums[mid]==0)
+                {
+                    
+                    low++;
+                    swap(nums[low],nums[mid]);
+                    mid++;
+                }
+                 else if (nums[mid]==1)
+                {
+                   mid++;
+                }
+                else{
+                    swap(nums[mid],nums[high]);
+                    high--;
+                    
+                }
+            }
+    }
+};
